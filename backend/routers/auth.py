922641dff4f45f046a38adb48c41f04b903730cc
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2023-12-10 21:09:29 +0200

    feat(backend): routers module implementing

---
+++ backend/routers/auth.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.get("/auth/")
+async def get_user():
+    return {'user': 'authenticated'}
---
+++ backend/routers/users.py
@@ -0,0 +1,76 @@
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+from pydantic import Field
+from typing import Annotated
+from fastapi import Depends, HTTPException, Path, APIRouter
+from models import Users
+from database import engine
+
+router = APIRouter()
+
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class UserRequest(BaseModel):
+    name: str = Field(min_length=3)
+    surname: str = Field(min_length=3)
+    banned: bool = Field(default=False)
+
+
+@router.get("/")
+async def root(db: db_dependency):
+    return db.query(Users).all()
+
+
+@router.get("/users")
+async def users(db: db_dependency):
+    return db.query(Users).all()
+
+
+@router.get("/users/{user_id}")
+async def user(db: db_dependency, user_id: int = Path(gt=0)):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if user_model is not None:
+        return user_model
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, user_request: UserRequest):
+    user_model = Users(**user_request.model_dump())
+
+    db.add(user_model)
+    db.commit()
+
+
+@router.put("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_user(db: db_dependency, user_id: int, user_request: UserRequest):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_model.name = user_request.name
+    user_model.surname = user_request.surname
+    user_model.banned = user_request.banned
+
+    db.add(user_model)
+    db.commit()
+
+
+@router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(db: db_dependency, user_id: int):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.query(Users).filter(Users.id == user_id).delete()
+    db.commit()
