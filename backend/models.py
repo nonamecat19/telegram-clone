afb615b2cfa4b4f942d1ce4447e340f05b57c4b6
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2023-12-10 22:32:50 +0200

    feat(backend): db chat improvements

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean
-
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.sql import func
 from database import Base
 
 
@@ -9,6 +9,27 @@ class Users(Base):
     name = Column(String)
     surname = Column(String)
     banned = Column(Boolean, default=False)
+    password = Column(String)
 
 
+class ChatRoom(Base):
+    __tablename__ = 'chat_room'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+
+class ChatMembers(Base):
+    __tablename__ = 'chat_members'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    chat_id = Column(Integer, ForeignKey('chats.id'))
+
+
+class ChatMessages(Base):
+    __tablename__ = 'chat_messages'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    chat_id = Column(Integer, ForeignKey('chat_room.id'))
+    text = Column(String)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
 
