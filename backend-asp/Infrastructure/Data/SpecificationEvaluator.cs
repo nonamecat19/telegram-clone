577f64b6e5b4817d0a133020c809b34247c2894d
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-16 21:00:18 +0200

    feat(asp): add specification for sorting

--- backend-asp/Infrastructure/Data/SpecificationEvaluator.cs
+++ backend-asp/Infrastructure/Data/SpecificationEvaluator.cs
@@ -15,6 +15,16 @@ public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
          query = query.Where(spec.Criteria);
       }
 
+      if (spec.OrderBy != null)
+      {
+         query = query.OrderBy(spec.OrderBy);
+      }
+      
+      if (spec.OrderByDescending != null)
+      {
+         query = query.OrderByDescending(spec.OrderByDescending);
+      }
+      
       query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
       return query;
    }
