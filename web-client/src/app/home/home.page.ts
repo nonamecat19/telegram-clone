4eff89c84869763f9d3d29b298ad6809cba00bde
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-19 15:03:54 +0200

    feat(frontend): add login service

--- web-client/src/app/home/home.page.ts
+++ web-client/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,15 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  private authService = inject(AuthService)
+
+  constructor() {
+    this.login()
+  }
+
+  login() {
+    this.authService.login('Name2', 'Secret').subscribe((data) => {
+      console.log(data)
+    })
+  }
 }
--- web-client/src/app/services/auth.service.ts
+++ web-client/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+const BASE_URL = "https://localhost:3001/api/v1"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private http = inject(HttpClient)
   constructor() { }
+
+  login(name: string, password: string) {
+    return this.http.post(`${BASE_URL}/auth/login`, {
+      Name: name,
+      Password: password
+    })
+  }
 }
